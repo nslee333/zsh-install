213547ec64cae037584003854611a945c6243136
Nathan <nslee333@gmail.com>
2025-04-12 13:19:51 -0700

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# vim ~/.zshrc
+# ZSH_THEME="agnoster"
+# source ~/.zshrc
